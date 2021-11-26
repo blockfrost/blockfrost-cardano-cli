9f6105e7452583f61e522ca471a564f02dbe4eee
slowbackspace <slowbackspace@gmail.com>
2021-11-26 21:01:57 +0100

    fix: keep object's keys sorted based on insertion order

--- src/utils/__tests__/format.test.ts
+++ src/utils/__tests__/format.test.ts
@@ -50,6 +50,7 @@ describe('format', () => {
   test('stringToBigInt', () => {
     expect(format.stringToBigInt(null)).toBeNull;
     expect(format.stringToBigInt('123')).toBe(BigInt(123));
+    expect(format.stringToBigInt('90071992547409910')).toBe(BigInt('90071992547409910'));
   });
 
   fixtures.transformPoolUpdateCert.forEach(f => {
